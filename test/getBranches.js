92cbad00e06de8025bcb13e77566746a9a94db78
Luke Karrys <lukekarrys@gmail.com>
2016-08-24 10:18:46 -0700

    dont rely on branches for tests

--- test/getBranches.js
+++ test/getBranches.js
@@ -6,7 +6,9 @@ const getBranches = require('../lib/getBranches')
 
 test('Finds the only master branch', (t) => {
   getBranches().then((branches) => {
-    t.deepEqual(branches, ['master'])
+    t.ok(Array.isArray(branches))
+    t.ok(branches.indexOf('master') > -1)
+    t.ok(branches.length)
     t.end()
   }).catch((err) => {
     t.notOk(err)
